2cc7ebecddda17945ea8ab19f95d7f5b303c8117
blanco <dehara14@gmail.com>
2025-02-21 17:20:42 -0600

    terminado

--- src/components/navigation/Header.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react"
-
-const Header:FC =()=>{
-    return <header className="navbar bg-base-100 sticky top-0 backdrop-blur-sm bg-base-100/80">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-    </header>
-}
-export default Header
