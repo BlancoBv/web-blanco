4bb1618a46e5e03e13d2f4419d329817f9653fe8
blanco <dehara14@gmail.com>
2026-03-05 18:27:30 -0600

    actualizado tailwind y añadida seccion para control de gastos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [vue({ template: { compilerOptions: { isCustomElement: (tag) => tag.startsWith("calendar") } } })],
   site: "https://programacion-blanco.com",
+  vite: { plugins: [tailwindcss()] }
 });
---
+++ src/components/controlGastos/WrapperInputs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import InputFecha from './InputFecha.vue';
+import InputMonto from './InputMonto.vue';
+import moment from 'moment';
+
+const date = moment()
+const fecha = ref<string>(date.format("YYYY-MM-DD"))
+
+</script>
+<template>
+    <div class="flex flex-col gap-4 mb-4">
+        <InputFecha v-model="fecha" />
+        <InputMonto label="gasto" :fecha="fecha" />
+        <InputMonto label="ingreso" :fecha="fecha" />
+    </div>
+</template>
