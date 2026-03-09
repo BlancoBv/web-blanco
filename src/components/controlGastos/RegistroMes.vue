8e53143e9307d9059ae64e5e4d811e86890e05c2
blanco <dehara14@gmail.com>
2026-03-08 18:56:06 -0600

    Cambio en lista de registros

--- src/components/controlGastos/RegistroMes.vue
+++ src/components/controlGastos/RegistroMes.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import moment from 'moment';
 import { computed, onMounted, ref } from 'vue';
+import categorias from 'src/utils/categorias';
 
 const isMounted = ref<boolean>(false)
 const gastos = ref<[string, number][]>([])
@@ -28,6 +29,14 @@ const deleteElement = (tipo: string, index: number) => {
 
         return
     }
+}
+
+const formatMoney = (value: number) => {
+    const f = new Intl.NumberFormat("es-MX", {
+        style: "currency",
+        currency: "MXN"
+    })
+    return f.format(value)
 }
 
 onMounted(() => {
@@ -58,20 +67,34 @@ onMounted(() => {
             <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Todos los registros del mes actual</li>
 
             <li class="list-row" v-for="(value, index) in lista">
-                <div class="text-4xl font-thin opacity-30 tabular-nums">{{ index + 1 }}</div>
-                <div>
+                <div class="text-4xl">
                     <template v-if="value[2] === 'gasto'">
-                        <i class="bi bi-arrow-up text-2xl text-error"></i>
+                        <i class="bi bi-arrow-up text-error"></i>
                     </template>
                     <template v-else>
-                        <i class="bi bi-arrow-down text-2xl text-success"></i>
+                        <i class="bi bi-arrow-down text-success"></i>
                     </template>
 
                 </div>
                 <div class="list-col-grow">
-                    <div>${{ value[1] }}</div>
-                    <div class="text-xs uppercase font-semibold opacity-60"> {{
-                        value[0].toString().split("T")[0] }}</div>
+                    <div class="flex gap-2 items-center">
+                        <i class="bi bi-cash text-lg"></i>
+                        <span>{{ formatMoney(value[1]) }}</span>
+                    </div>
+                    <div class="text-xs uppercase font-semibold grid grid-cols-1 grid-rows-2 gap-1">
+                        <div class="badge badge-soft badge-primary text-xs">
+                            <i class="bi bi-calendar"></i>
+                            <span>{{ value[0].toString().split("T")[0] }}</span>
+                        </div>
+                        <div v-if="value[2] === 'gasto'" class="badge badge-soft badge-secondary text-xs">
+                            <i class="bi bi-question-lg"></i>
+                            <span>
+                                {{ categorias[value[3] as keyof typeof categorias]?.nombre ??
+                                    "Indefinido"
+                                }}
+                            </span>
+                        </div>
+                    </div>
                 </div>
                 <button class="btn btn-square btn-ghost" @click="deleteElement(value[2], value.at(-1) as number)">
                     <i class="bi bi-trash"></i>
